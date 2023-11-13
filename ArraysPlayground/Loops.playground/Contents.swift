25c1329ca9969a23d105a68cf437d7884cf823fa
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-13 12:06:15 -0800

    Swift Bootcamp Notes

---
+++ ArraysPlayground/Loops.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Section 1.3: Dictionaries
+	/*
+	- All Keys need to be unique for their value
+	- Syntax to create a Dictonary:
+		var (dictionaryTypeName) = ["Key": "Value", "Key": "Value"]
+*/
+var nameToEmailDict = ["Trevis": "Trevis42@gmail.com", "Jarvis": "Jarvisfields@msn.com"]
+var blankDict = [String: String]()
+	// Mark: Dictionary Lookups
+let trevisEmail = nameToEmailDict("Trevis")
+	// Mark: Updating Dictionaries
+nameToEmailDict["Trevis"] = "Ftrevis@amazon.com"
+nameToEmailDict
+
