62011ff2ba368484ea7f811413898b62bb762782
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-14 01:14:25 -0800

    Remove Files
    Removing Files added ahead of knowledge level

--- ArraysPlayground/Optionals.playground/Contents.swift
+++ ArraysPlayground/Optionals.playground/Contents.swift
@@ -1,46 +1,64 @@
-import UIKit
-
 // MARK: Optionals
-var login = "Flash"
+/*
+- An optional value has two potiential states. It can either contain a value, or be nil (nothing)
+- Optional Values allow you to code more safely when  working with Properties that may or may not have a value
+- Examples can be: Phone Numbers, Email Addresses, Pictures (Avatars), Bio, Etc
+*/
+
+var username = "Flash"
 var bio: String?
-func getLoginInfo()
+
+func getUserInfo()
 {
-	print("Login: \(login)")
-	print("Bio: \(String(describing: bio))")
+	print("Username: \(username)")
+	print("Bio: \(bio)") // bio! is a FORCE unwrap feature that will cause program to crash
 	
+	/*
+		- Option 1 to unwrapping an optional value and extracting the value
+	*/
 	if let unwrappedBio = bio
 	{
-		print("User bio unwrapped is: \(unwrappedBio)")
+		print("Bio: \(unwrappedBio)") // Replaces Bio is Optional with Bio: Crimson Blur
 	}
-	print("-----------")
+	if bio != nil
+	{
+		print("Bio: \(bio!)")
+	}
+	print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
 }
-getLoginInfo()
-bio = "The Crimson Blur"
-getLoginInfo()
-//
-firstName = "John"
-var middleName = String?.self
-lastName = "Smith"
+getUserInfo() // Displays Bio is nil
+bio = "Crimson Blur" // Updates Bio to "Crimson Blur"
+getUserInfo() // Displays Bio is Optional("Crimson Blur")
+
+
+// MARK: - Default Values
+
+var firstName = "John"
+var middleName: String?
+var lastName = "Smith"
+
 func getFullName() -> String
 {
-	let middleUnwrapped = middleName
+	let middleUnwrapped = middleName ?? ""
 	return "\(firstName) \(middleUnwrapped) \(lastName)"
 }
+// Display Name without Middle Name entered
 getFullName()
-// Mark: Guard Statements
+// Update Middle name to Alex
+middleName = "Alex"
+// Display final result of full name
+getFullName()
+
+// MARK: - Guard Statements
 var email = "test@gmail.com"
 var phoneNumber: String?
+
 func sendConfirmation()
 {
-	print("Did call Function")
 	guard let number = phoneNumber
-	else
-	{ print("Send email confirmation to: \(email)")
-		return
-	}
-	print("Send text confirmation to number: \(number)")
+		else
+			{ return }
+	print("Send text confirmation to: \(number)")
 }
-sendConfirmation()
-phoneNumber = "555-555-5555"
-sendConfirmation()
 
+sendConfirmation() // Nothing is going to happen at this time
