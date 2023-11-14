03b567216d151ff116a2312558d735cad9c95484
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-14 13:49:58 -0800

    Skeleton code for App

--- ArraysPlayground/Exercises.playground/Contents.swift
+++ ArraysPlayground/Exercises.playground/Contents.swift
@@ -50,7 +50,7 @@ printOddOrEven(lowerBound: 0, upperBound: 100)
 
 
 
-// MARK:
+// MARK: Part 2 Arrays
 var nums = [1,2,3,4,5] // Array of numbers
 func combineSum() -> Int
 {
@@ -64,27 +64,89 @@ func combineSum() -> Int
 	return sum
 }
 
-func computeMax() -> Int {
+func computeMax() -> Int
+{
 	var max = 0
-	for num in nums {
-		if num > max {
+	for num in nums
+	{
+		print("Current Max: \(max)")
+		if num > max
+		{
 			max = num
+			print("Max: \(num)")
+		}
+		else
+		{
+			print("Num: \(num) is lower then the original max:\(max)")
 		}
 	}
 	return max
 }
 
-
-func computeMin() -> Int {
-	var min = 0
-	for num in nums {
-		if num < min {
+func computeMin() -> Int
+{
+	var min = Int.max
+	for num in nums
+	{
+		print("Current Min: \(min)")
+		if num < min
+		{
 			min = num
+			print("Min: \(num)")
 		}
+		
 	}
 	return min
 }
 
+
+
 // Call the instance of sum to display the total of the array
 combineSum()
 computeMax()
+computeMin()
+
+
+// MARK: Part 3 Guest List of users
+
+struct User
+{
+	let firstName: String
+	let lastName: String
+	let usename: String
+	
+	
+	let badge: String
+	let employeeID: String
+	
+	var level: String
+	var manager: String
+	var shiftCode: String
+	var stationCode: String
+	
+	var isUser: Bool
+	var isSuperUser: Bool
+	var isAdmin: Bool
+	
+}
+
+struct UserList
+{
+	var users: [User]
+	
+	func searchForUser(username: String) -> Bool
+	{
+		for user in users {
+			if username == user.usename
+			{
+				return true
+			}
+		}
+		return false
+	}
+}
+let user1 = User(firstName: "John", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: "T1", manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: false, isAdmin: false)
+let user2 = User(firstName: "Joe", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: "T1", manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: false)
+let user3 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: "T1", manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: true)
+
+let userList = UserList(users: [user1, user2, user3])
