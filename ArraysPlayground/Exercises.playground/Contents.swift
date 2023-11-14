255493fad82a2007fadd8a3affb28c1997fe3d71
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-14 15:06:40 -0800

    Code Cleaning

--- ArraysPlayground/Exercises.playground/Contents.swift
+++ ArraysPlayground/Exercises.playground/Contents.swift
@@ -22,34 +22,20 @@
 */
 
 // MARK: Part 1 Loop
-
-	// Create a dynamic lower and upper limit
-func printOddOrEven(lowerBound: Int, upperBound: Int)
-{
+func printOddOrEven(lowerBound: Int, upperBound: Int) { 	// Create a dynamic lower and upper limit
 	// Run a For loop that will print out even or odd numbers
-	for i in lowerBound...upperBound
-	{
-		if i % 2 == 0 // % is modulus or produce a remainder
-		{
+	for i in lowerBound...upperBound {
+		if i % 2 == 0 { // % is modulus or produce a remainder
 		print(i)
 	}
-		else
-		{
-			if i % 2 != 0 // remainder (!=) not equal to zero
-			{
+		else {
+			if i % 2 != 0 { // remainder (!=) not equal to zero
 				print(i)
 			}
 		}
 		print("---------")
 	}
 }
-printOddOrEven(lowerBound: 0, upperBound: 20)
-printOddOrEven(lowerBound: 10, upperBound: 30)
-printOddOrEven(lowerBound: 0, upperBound: 50)
-printOddOrEven(lowerBound: 0, upperBound: 100)
-
-
-
 // MARK: Part 2 Arrays
 var nums = [1,2,3,4,5] // Array of numbers
 func combineSum() -> Int
@@ -63,7 +49,6 @@ func combineSum() -> Int
 	}
 	return sum
 }
-
 func computeMax() -> Int
 {
 	var max = 0
@@ -82,7 +67,6 @@ func computeMax() -> Int
 	}
 	return max
 }
-
 func computeMin() -> Int
 {
 	var min = Int.max
@@ -98,17 +82,7 @@ func computeMin() -> Int
 	}
 	return min
 }
-
-
-
-// Call the instance of sum to display the total of the array
-combineSum()
-computeMax()
-computeMin()
-
-
 // MARK: Part 3 Guest List of users
-
 struct User
 {
 	let firstName: String
@@ -119,7 +93,7 @@ struct User
 	let badge: String
 	let employeeID: String
 	
-	var level: String
+	var level: Int
 	var manager: String
 	var shiftCode: String
 	var stationCode: String
@@ -129,27 +103,22 @@ struct User
 	var isAdmin: Bool
 	
 }
-
 struct UserList
 {
 	var users: [User]
-	
-	func searchForUser(username: String) -> Bool
-	{
-		for user in users {
-			print("Searching. . .")
-			if username.lowercased() == user.usename.lowercased()
-			{
-				print("User has signed up!")
-				return true
-			}
-		}
-		print("Please Sign up to begin using the App today!")
-		return false
-	}
-	
-	func getUsersManager (manager: String) -> [User]
-	{
+	func searchForUser(username: String) -> Bool {
+for user in users {
+		 print("Searching. . .")
+		 if username.lowercased() == user.usename.lowercased()
+		 {
+		 print("User has signed up!")
+		 return true
+		 }
+}
+print("Please Sign up to begin using the App today!")
+return false
+}
+	func getUsersManager (manager: String) -> [User] {
 		var results = [User]()
 		for user in users
 		{
@@ -160,16 +129,60 @@ struct UserList
 		}
 		return results
 	}
-	
+	func getActiveUsers() -> Int {
+		var count = 0
+		for user in users {
+			if user.isUser {
+				count += 1
+			}
+		}
+		return count
+	}
+	func getInactiveUsers() -> Int {
+		var count = 0
+		for user in users
+		{
+			if !user.isUser {
+				count += 1
+			}
+		}
+		return count
+	}
+	func printReport() {
+		print("Active Users: \(getActiveUsers())")
+		print("Inactive Users: \(getInactiveUsers())")
+		let tier1Associate = getActiveUsers(isUser: 1)
+		let
+		print("Total Users for the site: \(0)")
+	}
 }
-let user1 = User(firstName: "John", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: "T1", manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: false, isAdmin: false)
-let user2 = User(firstName: "Joe", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: "T1", manager: "Tom", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: false)
-let user3 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: "T1", manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: true)
 
+// MARK: Declartions
+let user1 = User(firstName: "John", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 1, manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: false, isAdmin: false)
+let user2 = User(firstName: "Joe", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 3, manager: "Tom", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: false)
+let user3 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 1, manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: true)
+let user4 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 5, manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: true)
+let user5 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 4, manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: false, isSuperUser: false, isAdmin: false)
+let user6 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 6, manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: true, isSuperUser: true, isAdmin: true)
+let user7 = User(firstName: "Jack", lastName: "Smith", usename: "Jsmith", badge: "1234567", employeeID: "909912134", level: 7, manager: "Jeff", shiftCode: "NNNNTTTT", stationCode: "XXX#", isUser: false, isSuperUser: false, isAdmin: false)
 let userList = UserList(users: [user1, user2, user3])
-
-
-userList.searchForUser(username: "John Smith")
 let manager1 = userList.getUsersManager(manager: "Jeff")
 let manager2 = userList.getUsersManager(manager: "Tom")
+let active = userList.getActiveUsers()
+let notActive = userList.getInactiveUsers()
 
+
+
+// MARK: Calls
+printOddOrEven(lowerBound: 0, upperBound: 20)
+printOddOrEven(lowerBound: 10, upperBound: 30)
+printOddOrEven(lowerBound: 0, upperBound: 50)
+printOddOrEven(lowerBound: 0, upperBound: 100)
+
+
+combineSum()
+computeMax()
+computeMin()
+
+userList.searchForUser(username: "John Smith")
+userList.printReport()
