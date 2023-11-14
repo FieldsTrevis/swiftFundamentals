690a8c0a0382e0d3b99e57d9db76228bcceaf64b
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-14 11:56:33 -0800

    Array Exercise

--- ArraysPlayground/Exercises.playground/Contents.swift
+++ ArraysPlayground/Exercises.playground/Contents.swift
@@ -47,3 +47,44 @@ printOddOrEven(lowerBound: 0, upperBound: 20)
 printOddOrEven(lowerBound: 10, upperBound: 30)
 printOddOrEven(lowerBound: 0, upperBound: 50)
 printOddOrEven(lowerBound: 0, upperBound: 100)
+
+
+
+// MARK:
+var nums = [1,2,3,4,5] // Array of numbers
+func combineSum() -> Int
+{
+	var sum = 0
+	for num in nums
+	{
+		print("Sum Before: \(sum)")
+		sum += num
+		print("Sum After: \(sum)")
+	}
+	return sum
+}
+
+func computeMax() -> Int {
+	var max = 0
+	for num in nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+
+func computeMin() -> Int {
+	var min = 0
+	for num in nums {
+		if num < min {
+			min = num
+		}
+	}
+	return min
+}
+
+// Call the instance of sum to display the total of the array
+combineSum()
+computeMax()
