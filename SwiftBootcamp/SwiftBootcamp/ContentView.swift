24309ee8c4d6bc904b47714203ed873a629e1e3e
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-14 21:01:06 -0800

    SwiftUI Bootcamp

---
+++ SwiftBootcamp/SwiftBootcamp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  SwiftBootcamp
+//
+//  Created by Trevis Jontaé Fields on 11/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      
+			ZStack {
+					HStack {
+						VStack {
+							HStack {
+									Rectangle()
+										.frame(width: 50, height: 50)
+								
+								}
+							}
+					
+						}
+		 			}
+			
+		 	}
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftBootcamp/SwiftBootcamp/SwiftBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootcampApp.swift
+//  SwiftBootcamp
+//
+//  Created by Trevis Jontaé Fields on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
