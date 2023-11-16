69dc213c3710772889d04d253f9f45f5352ba5a0
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-15 17:56:45 -0800

    SwiftUI Skeleton

--- SwiftBootcamp/SwiftBootcamp/ContentView.swift
+++ SwiftBootcamp/SwiftBootcamp/ContentView.swift
@@ -10,25 +10,125 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
       
+		
+				//	HStack {
+//						// VStack(alignment: .leading, spacing:  10) {
+//							ZStack {
+//								Rectangle()
+//									.fill(Color.blue)
+//									.frame(width: 50, height: 50)
+//									.cornerRadius(30.0)
+//								Image(systemName: "tray.fill")
+//									.foregroundColor(.white)
+//									.font(.headline)
+//							}
+//							ZStack {
+//								Rectangle()
+//									.fill(Color.blue)
+//									.frame(width: 50, height: 50)
+//									.cornerRadius(30.0)
+//								Image(systemName: "swift")
+//									.foregroundColor(.white)
+//									.font(.headline)
+//							}
+//							ZStack {
+//								Rectangle()
+//									.fill(Color.blue)
+//									.frame(width: 50, height: 50)
+//									.cornerRadius(30.0)
+//								Image(systemName: "sidebar.right")
+//									.foregroundColor(.white)
+//									.font(.headline)
+//							}
+//							ZStack {
+//								Rectangle()
+//									.fill(Color.blue)
+//									.frame(width: 50, height: 50)
+//									.cornerRadius(30.0)
+//								Image(systemName: "cart.fill")
+//									.foregroundColor(.white)
+//									.font(.headline)
+//							}
+//									Text("Check recent purchases")
+//										.foregroundColor(.red)
+//										.font(.caption2)
+//
+//							Divider()
+//
+//							}
+						//.padding()
 			ZStack {
-					HStack {
-						VStack {
-							HStack {
-									Rectangle()
-										.frame(width: 50, height: 50)
-								
-								}
+			LazyVStack() {
+				Capsule()
+					.stroke(Color.red)
+					.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .topLeading)
+					.padding()
+				Capsule()
+					.stroke(Color.yellow)
+					.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .topLeading)
+					.padding()
+				Capsule()
+					.stroke(Color.green)
+					.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .topLeading)
+					.padding()
+			}
+						HStack(alignment: .bottom,spacing: 35) {
+							Divider()
+							ZStack {
+								Rectangle()
+									.fill(Color.blue)
+									.frame(width: 50, height: 50)
+									.cornerRadius(30.0)
+								Image(systemName: "tray.fill")
+									.foregroundColor(.white)
+									.font(.headline)
 							}
+							ZStack {
+								Rectangle()
+									.fill(Color.blue)
+									.frame(width: 50, height: 50)
+									.cornerRadius(30.0)
+								Image(systemName: "swift")
+									.foregroundColor(.white)
+									.font(.headline)
+							}
+							ZStack {
+								Rectangle()
+									.fill(Color.blue)
+									.frame(width: 50, height: 50)
+									.cornerRadius(30.0)
+								Image(systemName: "sidebar.left")
+									.foregroundColor(.white)
+									.font(.headline)
+							}
+							ZStack {
+								Rectangle()
+									.fill(Color.blue)
+									.frame(width: 50, height: 50)
+									.cornerRadius(30.0)
+								Image(systemName: "cart.fill")
+									.foregroundColor(.white)
+									.font(.headline)
+							}
+//									Text("Check recent purchases")
+//										.foregroundColor(.red)
+//										.font(.caption2)
+							Divider()
+							}
+						.padding()
+							Spacer()
 					
-						}
-		 			}
-			
+						//}
+			}
 		 	}
     }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+			Group {
+				ContentView()
+				ContentView()
+			}
     }
 }
