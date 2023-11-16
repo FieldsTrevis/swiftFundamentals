2c62d8e20bee8deddaa1d1c3cc044e3e3ed0c18d
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-15 19:44:17 -0800

    Skeleton of SwiftUI

--- SwiftBootcamp/SwiftBootcamp/ContentView.swift
+++ SwiftBootcamp/SwiftBootcamp/ContentView.swift
@@ -58,55 +58,103 @@ struct ContentView: View {
 //							}
 						//.padding()
 			ZStack {
-			LazyVStack() {
+				VStack(alignment: .leading) {
+				HStack(alignment: .top) {
+				VStack(alignment: .leading) {
+					
+					ZStack {
+					Capsule()
+						.stroke(Color.red, lineWidth: 4)
+						.frame(width: 90, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+					VStack {
+						HStack {
+							ZStack {
+							Image(systemName: "xmark.circle")
+								.resizable()
+								.frame(width: 25, height: 25, alignment: .leading)
+						}
+						Text("EXIT")
+							.font(.headline)
+							.fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+						}
+						HStack {
+							ZStack {
+							Image(systemName: "figure")
+								.resizable()
+								.frame(width: 25, height: 25, alignment: .leading)
+						}
+						Text("EXIT")
+							.font(.headline)
+							.fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+						}
+						HStack {
+							ZStack {
+							Image(systemName: "xmark.circle")
+								.resizable()
+								.frame(width: 25, height: 25, alignment: .leading)
+						}
+						Text("EXIT")
+							.font(.headline)
+							.fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+						}
+					}
+				}
+				
+					
+					Capsule()
+					.stroke(Color.yellow, lineWidth: 15)
+					.frame(width: 25, height: 25, alignment: .topLeading)
+					.padding(6)
 				Capsule()
-					.stroke(Color.red)
-					.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .topLeading)
-					.padding()
-				Capsule()
-					.stroke(Color.yellow)
-					.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .topLeading)
-					.padding()
-				Capsule()
-					.stroke(Color.green)
-					.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .topLeading)
-					.padding()
+					.stroke(Color.green, lineWidth: 15)
+					.frame(width: 25, height: 25, alignment: .topLeading)
+					.padding(6)
 			}
+				.padding()
+					Spacer()
+				}
+					Spacer()
+				}
+				
 						HStack(alignment: .bottom,spacing: 35) {
 							Divider()
 							ZStack {
 								Rectangle()
-									.fill(Color.blue)
-									.frame(width: 50, height: 50)
-									.cornerRadius(30.0)
-								Image(systemName: "tray.fill")
-									.foregroundColor(.white)
-									.font(.headline)
-							}
-							ZStack {
-								Rectangle()
-									.fill(Color.blue)
+									.fill(Color.green)
 									.frame(width: 50, height: 50)
 									.cornerRadius(30.0)
 								Image(systemName: "swift")
+									.imageScale(.large)
 									.foregroundColor(.white)
 									.font(.headline)
 							}
 							ZStack {
 								Rectangle()
-									.fill(Color.blue)
+									.fill(Color.yellow)
 									.frame(width: 50, height: 50)
 									.cornerRadius(30.0)
-								Image(systemName: "sidebar.left")
+								Image(systemName: "square.stack.3d.down.forward")
+									.imageScale(.large)
+									.foregroundColor(.black)
+									.font(.headline)
+							}
+							ZStack {
+								Rectangle()
+									.fill(Color.orange)
+									.frame(width: 50, height: 50)
+									.cornerRadius(30.0)
+								Image(systemName: "filemenu.and.selection")
+									.imageScale(.large)
 									.foregroundColor(.white)
 									.font(.headline)
 							}
 							ZStack {
 								Rectangle()
-									.fill(Color.blue)
+									.fill(Color.red)
 									.frame(width: 50, height: 50)
 									.cornerRadius(30.0)
-								Image(systemName: "cart.fill")
+								Image(systemName: "folder.fill")
+									.imageScale(.large)
 									.foregroundColor(.white)
 									.font(.headline)
 							}
