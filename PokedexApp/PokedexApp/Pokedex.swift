97b13bf0a4168ec8d0ecc20a7ec8cbfd267c11ac
Trevis J. Fields <TREVISFIELDS@MSN.COM>
2023-11-27 13:43:28 -0800

    Broken App Code

---
+++ PokedexApp/PokedexApp/Pokedex.swift
@@ -0,0 +1,20 @@
+//
+//  Pokedex.swift
+//  PokedexApp
+//
+//  Created by Trevis Jontaé Fields on 11/27/23.
+//
+
+import SwiftUI
+
+struct Pokedex: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Pokedex_Previews: PreviewProvider {
+    static var previews: some View {
+        Pokedex()
+    }
+}
---
+++ PokedexApp/PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Trevis Jontaé Fields on 11/27/23.
+//
+
+import SwiftUI
+
+@main struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Pokedex()
+        }
+    }
+}
